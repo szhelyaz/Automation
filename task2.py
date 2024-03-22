adea5a652bf5c5a338eb5bb114870a1d3debdaf8
szhelyaz <145033673+szhelyaz@users.noreply.github.com>
2024-03-22 13:42:19 +0200

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,11 @@
+fname = input("Input your First Name : ")
+
+lname = input("Input your Last Name : ")
+
+sume = input("Input your sume : ")
+
+x = float(sume)
+
+print("Hello " + fname + " " + lname + ". You have "  + f'${x}' + " in your account.")
+
+
